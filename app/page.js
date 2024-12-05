b0bf48d634997892b40cf705eb17ebc92bb1cb6e
Subhampreet <subhampreet18@gmail.com>
2024-12-05 18:35:07 +0530

    Build 0.0.2 : Kinde Auth Integrated

--- app/page.js
+++ app/page.js
@@ -1,9 +1,16 @@
 import { ModeToggle } from "./components/header";
+import { RegisterLink, LoginLink } from "@kinde-oss/kinde-auth-nextjs/components";
+
 
 export default function Home() {
   return (
     <div>
       <ModeToggle />
+
+
+      <LoginLink>Sign in</LoginLink>
+
+      <RegisterLink>Sign up</RegisterLink>
     </div>
   );
 }
