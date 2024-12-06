ec1d9fba0930ae7859bca3c14d62f189441526de
Subhampreet <subhampreet18@gmail.com>
2024-12-06 23:41:21 +0530

    Build 0.0.8 : Header Items Alignment

--- app/components/header.js
+++ app/components/header.js
@@ -16,9 +16,9 @@ export default function Header() {
       <header className="flex h-20 w-full shrink-0 items-center px-4 md:px-6">
         <Link href="/" className="mr-6 hidden lg:flex items-center gap-2" prefetch={false}>
           <Image src={Logo} alt="Logo" className="w-14 hidden lg:block" />
-          <div className={`${coveredByYourGrace.className} text-2xl`}>buy me a pizza</div>
+          <div className={`${coveredByYourGrace.className} text-2xl tracking-normal`}>buy me a pizza</div>
         </Link>
-        <div className="ml-auto flex gap-4">
+        <div className="ml-auto flex gap-4 items-center">
           <Link
             href="#"
             className="group inline-flex h-9 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-gray-100/50 data-[state=open]:bg-gray-100/50 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus:bg-gray-800 dark:focus:text-gray-50 dark:data-[active]:bg-gray-800/50 dark:data-[state=open]:bg-gray-800/50"
@@ -33,6 +33,7 @@ export default function Header() {
           >
             About
           </Link>
+          
           <LoginLink>
             <Button variant="outline" className="justify-self-end px-5 py-5 text-sm rounded-full">
               Sign in
